28fa6f0543f2d46aa96134ebdd91872c4f0c566d
shakaibakhan-afk <shakaiba.khan@wamolabs.com>
2025-12-19 17:37:30 +0500

    add a mul fun

---
+++ multiply.py
@@ -0,0 +1,9 @@
+def multiply(a, b):
+    """Returns the product of two numbers"""
+    return a * b
+
+# Test the function
+if __name__ == "__main__":
+    x = 6
+    y = 7
+    print(f"{x} * {y} = {multiply(x, y)}")
